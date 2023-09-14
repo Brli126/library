61fb70ea6ad8b99a2a7c2b36549310510c5c4851
Brli126 <zy972822@gmail.com>
2023-09-13 23:29:00 -0400

    Finish the design of card.

--- mylib.js
+++ mylib.js
@@ -10,8 +10,10 @@ function Book(title, author, npage, isRead) {
     };
 }
 
+// Every book in the library will be display as a card
 
 function createCard(idx) {
+    // create DOM for cards.
     const card = document.createElement('div');
     const title = document.createElement('div');
     const author = document.createElement('div');
@@ -24,9 +26,34 @@ function createCard(idx) {
     card.appendChild(author);
     card.appendChild(page);
 
+    // CSS styling for the card elements just created
+
     btnContainer.style.display = 'flex';
-    btnContainer.style['justify-content'] = 'center';
+    btnContainer.style.gap = '30px';
     card.appendChild(btnContainer);
+
+
+    // CSS for remove button and read button
+
+    removeBtn.style.flex = '1';
+    removeBtn.style.height = '40px';
+    removeBtn.style.border = 'none';
+    removeBtn.style.borderRadius = '20px';
+    removeBtn.style.backgroundColor = '#ef4444';
+    removeBtn.style.color = 'white';
+    removeBtn.textContent = 'Remove'
+    removeBtn.style.fontSize = '20px';
+
+    readBtn.style.flex = '1';
+    readBtn.style.height = '40px';
+    readBtn.style.border = 'none';
+    readBtn.style.borderRadius = '20px';
+    readBtn.style.backgroundColor = '#0ea5e9';
+    readBtn.style.color = 'white';
+    readBtn.textContent = 'Read';
+    readBtn.style.fontSize = '20px';
+    
+
 
 
     btnContainer.appendChild(removeBtn);
@@ -35,28 +62,38 @@ function createCard(idx) {
     card.classList.add('card');
     card.setAttribute('data-index', `'${idx}'`);
 
-    const myNewBook = myLibrary[idx]
+    const myNewBook = myLibrary[idx];
     title.textContent = myNewBook.title;
     author.textContent = myNewBook.author;
-    page.textContent = myNewBook.npage;
+    page.textContent = `${myNewBook.npage} pages`;
 
-    card.style.height = '200px';
+
+    // Style for the "author" and "number of page" text
+    author.style.fontSize = '20px';
+    page.style.fontSize = '20px'
+
+    // CSS for .card elements
+
+    card.style.height = '250px';
+    card.style.padding = '20px';
+    card.style['font-size'] = '25px';
+    card.style['text-align'] = 'center'
     card.style.backgroundColor = 'white';
-    card.style.display = 'flex';
-    card.style['flex-flow'] = 'column';
-    card.style['align-content'] = 'center';
+    card.style.display = 'grid';
+    card.style.gap = '30px';
+    card.style['grid-templete-row'] = 'repeat(4, 1fr)';
+    card.style['boxShadow'] = '4px 4px 2px -2px rgba(0, 0, 0, 0.1)';
+    card.style.borderRadius = '10px';
+
 
     return card;
 }
 
-function displayCard() {
-    const contentArea = document.querySelector('#content');
-    
-    for (let i = 0; i < myLibrary.length; i++) {
-        let newCard = createCard(i);
-        contentArea.appendChild(newCard);
+function displayCard(idx) {
 
-    }
+    const contentArea = document.querySelector('#content');
+    let newCard = createCard(idx);
+    contentArea.appendChild(newCard);
 
 }
 
@@ -101,8 +138,7 @@ function addBookToLibrary() {
     
 
     addDialog.addEventListener('close', (e) => {
-        console.log(myLibrary[0]);
-        displayCard();
+        displayCard(myLibrary.length-1);
     })
    
 
